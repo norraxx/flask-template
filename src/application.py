632f45a879a1a1c7f8e23e9d78e620f405e87588
Konstantin Kalinovsky <k_kalinovsky@wargaming.net>
2024-01-18 15:56:18 +0100

    simpliest form request

--- src/application.py
+++ src/application.py
@@ -1,11 +1,8 @@
 # -*- encoding: utf-8 -*-
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
 app.config.from_object('src.configuration.Config')
-
-db = SQLAlchemy(app)
--- src/views.py
+++ src/views.py
@@ -1,10 +1,28 @@
 # -*- encoding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request
 
 from .application import app
 
 
 @app.route('/')
 def index():
-    return render_template("pages/index.html")
+    return render_template(
+        "pages/index.html",
+        key='value1&'
+    )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return render_template(
+            "pages/login.html",
+            login=request.form.get('login'),
+            password=request.form.get('password'),
+        )
+
+    return render_template(
+        "pages/login.html",
+    )
+
